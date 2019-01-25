e1179503427dc79856d89e61dfad610d5a3ad7fc
William M <wmooney1984@gmail.com>
2019-01-24 21:31:12 -0500

    DOM Manipulation: Completed

--- Section 15/main.js
+++ Section 15/main.js
@@ -1,1 +1,15 @@
-alert("Working");
+let h1 = document.querySelector("h1");
+
+h1.style.color = "pink";
+
+let body = document.querySelector("body");
+let isBlue = false;
+
+setInterval(function() {
+  if (isBlue) {
+    body.style.background = "white";
+  } else {
+    body.style.background = "#3498db";
+  }
+  isBlue = !isBlue;
+}, 1000);
